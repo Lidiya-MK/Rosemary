678fa1521b0cd653d0cdf7318f037e5eccd47d97
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2025-08-15 14:02:31 +0300

    added appbar

--- rosemary/lib/pages/home_screen.dart
+++ rosemary/lib/pages/home_screen.dart
@@ -7,8 +7,14 @@ const HomeScreen({super.key});
 @override
   Widget build(BuildContext context) {
     return (
-Scaffold(appBar: AppBar(leading: Image.asset("assets/logo.png"),))
-
+Scaffold(
+  appBar: AppBar(
+    leading: Padding(child: Image.asset("assets/logo.png",), padding:EdgeInsets.only(left: 20, ),),
+    leadingWidth: 56,
+    title:Padding(padding: EdgeInsets.only(left: 0), child:Text("RoseMary", style: GoogleFonts.josefinSans(fontSize: 24),) ,),
+    actions: [Padding(padding: EdgeInsets.only(right: 20),child: Icon(Icons.menu),)],
+  ),
+)
     );
   }
 
