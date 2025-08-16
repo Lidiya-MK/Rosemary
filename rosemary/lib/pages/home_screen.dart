499a232be5f1c58276ab0d97ed5dccf4fe6fa97a
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2025-08-16 18:45:17 +0300

    added categories

--- rosemary/lib/pages/home_screen.dart
+++ rosemary/lib/pages/home_screen.dart
@@ -19,35 +19,62 @@ Scaffold(
 body: Column(
   
   children: [
-    Padding(padding:EdgeInsets.all(24),child: Card(
+    Padding(padding:EdgeInsets.all(24),child:
+     Card(
+      elevation: 24,
       color: Color(0xFF14AA87),
       
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-
-          Text("Welcome", style: GoogleFonts.josefinSans(fontSize: 24, color: Colors.white),),
+Padding(padding: EdgeInsets.only(left: 16, top: 16), child: Text("Welcome", style: GoogleFonts.josefinSans(fontSize: 24, color: Colors.white),),)
+          ,
           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("For centuries, people around\nthe world have turned to plants,\n seeds, and herbs for healing,\nbeauty, and nourishment. ", style: GoogleFonts.josefinSans(fontSize: 15, color: Colors.white),),
+              Text("For centuries, people around\nthe world have turned to plants,\n seeds, and herbs for healing,\nbeauty, and nourishment. ", style: GoogleFonts.josefinSans(fontSize: 12, color: Colors.white),),
+              SizedBox(width: 16,),
               Image.asset("assets/herbs0.png", height: 75,)
 
             ],
           ),
+          SizedBox(height: 10,),
             Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Image.asset("assets/spice0.png", height: 75,),
-              Text("From soothing skin to boosting \nwellness, these natural treasures \nhave stood the test of time and\nnow, we bring their stories and\nbenefits to your fingertips. ", style: GoogleFonts.josefinSans(fontSize: 15, color: Colors.white),),
+            SizedBox(width: 16,),
+              Text("From soothing skin to boosting \nwellness, these natural treasures \nhave stood the test of time and\nnow, we bring their stories and\nbenefits to your fingertips. ", style: GoogleFonts.josefinSans(fontSize: 12, color: Colors.white),),
               
 
             ],
-          )
+          ),
+          SizedBox(height: 16,),
         ],
       ),
       
-    ),),
-    
+    ),
+    ),
+    Padding(padding: EdgeInsets.only(left:14 ), child:Row(children: [
+      Image.asset("assets/categories.png"),
+      Text("Categories", style: GoogleFonts.josefinSans(fontSize: 35),)
+    ],), ),
+   
+    Row(children: [
+      
+     Padding(padding: EdgeInsets.all(14), child:  Card(color: Color(0xFF14AA87),child:Padding(padding: EdgeInsets.all(18), child: Row(children: [Image.asset("assets/herb_white.png", height: 24,), Text("Herbs", style: GoogleFonts.josefinSans(fontSize: 20, color: Colors.white),)],)) ),),
+         Padding(padding: EdgeInsets.all(14), child:  Card(color: Color(0xFF14AA87),child:Padding(padding: EdgeInsets.all(18), child: Row(children: [Image.asset("assets/seed_white.png", height: 32,), Text("Seeds", style: GoogleFonts.josefinSans(fontSize: 20, color: Colors.white),)],)) ),)
+    ],),
+     Row(children: [
+      
+     Padding(padding: EdgeInsets.all(14), child:  Card(color: Color(0xFF14AA87),child:Padding(padding: EdgeInsets.all(18), child: Row(children: [Image.asset("assets/cereal_white.png", height: 32,), Text("Cereals", style: GoogleFonts.josefinSans(fontSize: 20, color: Colors.white),)],)) ),),
+         Padding(padding: EdgeInsets.all(14), child:  Card(color: Color(0xFF14AA87),child:Padding(padding: EdgeInsets.all(18), child: Row(children: [Image.asset("assets/f&v_white.png", height: 32,), Text("Fruits & \nVegetables", style: GoogleFonts.josefinSans(fontSize: 15, color: Colors.white ,),textAlign: TextAlign.center,)],)) ),)
+    ],)
   ],
 ),
+
+
 )
     );
   }
